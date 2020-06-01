a09f3455586e81f1946a9ff670cd8fe503f8ea49
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-06-01 19:52:36 -0300

    Configuração Inicial Web

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -3,12 +3,8 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-
     response.json([
         'Jp',
-        'Felipe',
-        'Cesar'
     ]);
 });
 
