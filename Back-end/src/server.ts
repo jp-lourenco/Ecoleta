6f19eef44a7486214f3511bf27335cefca7b6efc
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-06-02 19:08:24 -0300

    Salvando Alterações

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -1,11 +1,35 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+const users = [
+    'Jp',
+    'Joao',
+    'Pedro'
+]
+
 app.get('/users', (request, response) => {
-    response.json([
-        'Jp',
-    ]);
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'joao pedro',
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
