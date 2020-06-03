e06a091ccb1b29937c686dc4a96b7ca5432c889c
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-06-03 20:55:10 -0300

    Salvando Alterações

--- Back-end/src/routes.ts
+++ Back-end/src/routes.ts
@@ -1,9 +1,47 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import knex from './database/connection';
+
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({"message":"oi"});
+routes.get('/itens', async (request, response) => {
+    const itens = await knex('itens').select('*');
+
+    const serializedItens = itens.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http:localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(serializedItens);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        cidade,
+        uf,
+        itens
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        cidade,
+        uf,
+    });
+
+    return response.json({sucess:'true'})
 });
 
 export default routes;
