2ab25a918f763ad8b11d66f621bd55cf78a7e525
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-06-02 21:59:43 -0300

    Configuração inicial do sqlite3

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -1,35 +1,10 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes';
+
 
 const app = express();
 
-const users = [
-    'Jp',
-    'Joao',
-    'Pedro'
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const user = {
-        name: 'joao pedro',
-    };
-
-    return response.json(user);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
